85f265e0ef00f48e661d91ebf03d68eb2954e4de
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-01-22 14:21:20 -0600

    Added comments describing the code functionality in client.js

--- public/client.js
+++ public/client.js
@@ -1,19 +1,27 @@
 $(function() {
   console.log('document loaded');
-
+  // all four of the 'on click' event handlers used for:
+  // numbers, operators, equal, and clear respectively.
   $('.input').on('click', typeInput);
   $('.operation').on('click', typeOperation);
   $('#calculator').on('click', '#equals', calculate);
   $('#clear').on('click', clear);
 });
+// inputCount is used to specify which of the three spans to append the value on.
+// first span : input1(first value), second span : input2(operator), third span : input3(second value).
 var inputCount = 1;
+// operator keeps track of which operation is going to be used in the 'typeOperation' function.
 var operator = '';
 
+// whenever this function is called, it appends the value clicked to the selected span.
 function typeInput(event) {
   event.preventDefault();
   $('#input' + inputCount).append($(this).attr('id'));
 }
 
+// whenever this function is called it saves the operation id
+// into 'operator', increments the inputCount by 1, appends the operation into the second span,
+// and finally adds 1 to inputCount.
 function typeOperation(event) {
   event.preventDefault();
   if (inputCount == 1) {
@@ -24,6 +32,9 @@ function typeOperation(event) {
   }
 }
 
+// converts #input1 and #input2's text into url encoded data to send to the server.
+// also chooses which route to send the data to by appending the operation name
+// on the URL.
 function calculate(event) {
   event.preventDefault();
   if (inputCount == 3) {
@@ -41,6 +52,7 @@ function calculate(event) {
   }
 }
 
+// Makes a GET request to obtain the value on the server side, then runs appendResult.
 function showResult() {
   $.ajax({
     url: '/calc',
@@ -49,6 +61,9 @@ function showResult() {
   });
 }
 
+// Removes the previous class 'answer' div if there was one on the DOM.
+// Appends a new div of class 'answer' with the result of the calculation onto the #display div.
+// On the next click of any button on the calculator, it clears the screen.
 function appendResult(obj) {
   $('.answer').remove();
   $('#display').append('<div class="answer">'+ '= ' + obj.result +'</div>');
@@ -57,7 +72,7 @@ function appendResult(obj) {
   }
 }
 
-
+// Clears the screen and removes the div with class 'answer'.
 function clear() {
   $('.answer').remove();
   $('.screen').text('');
